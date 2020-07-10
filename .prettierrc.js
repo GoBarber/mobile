9b6e474f6bd3502260be25110184aeae85a6581d
Bernardo Henrique <bernardohrl.unb@gmail.com>
2020-07-09 22:32:01 -0300

    Adds New Projects

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,7 @@
 module.exports = {
-  bracketSpacing: false,
   jsxBracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
+  printWidth: 120,
+  arrowParens: "avoid"
 };
--- src/index.js
+++ src/index.js
@@ -1,32 +1,38 @@
-import React, {useEffect, useState} from 'react';
-import {
-  SafeAreaView,
-  FlatList,
-  Text,
-  StyleSheet,
-  StatusBar,
-} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
 import api from './services/api.js';
 
 export default function App() {
   const [projects, setProjects] = useState([]);
 
   useEffect(() => {
-    api.get('projects').then((response) => {
+    api.get('projects').then(response => {
       setProjects(response.data);
     });
   }, []);
+
+  async function handleAddProject() {
+    const response = await api.post('projects', {
+      title: `Novo Projeto ${Date.now()}`,
+      owner: 'Bernardo Henrique',
+    });
+
+    const project = response.data;
+    setProjects([...projects, project]);
+  }
+
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
         <FlatList
           data={projects}
-          keyExtractor={(project) => project.id}
-          renderItem={({item: project}) => (
-            <Text style={styles.title}>{project.title}</Text>
-          )}
+          keyExtractor={project => project.id}
+          renderItem={({ item: project }) => <Text style={styles.title}>{project.title}</Text>}
         />
+        <TouchableOpacity activeOpacity={0.6} style={styles.button} onPress={handleAddProject}>
+          <Text style={styles.buttonText}>Adicionar Projeto</Text>
+        </TouchableOpacity>
       </SafeAreaView>
     </>
   );
@@ -41,5 +47,17 @@ const styles = StyleSheet.create({
     color: '#FFF',
     fontSize: 50,
     fontWeight: 'bold',
+  },
+  button: {
+    backgroundColor: '#FFF',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 });
