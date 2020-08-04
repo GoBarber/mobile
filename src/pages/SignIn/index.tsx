03f2cc45f1b15a201af04e3794ca8f5adcfc2950
Bernardo Henrique <bernardohrl.unb@gmail.com>
2020-08-03 23:15:31 -0300

    Authentication Page Navigation
    Adds links to other pages.

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import { Image } from 'react-native';
+import {
+  Image,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/input';
-import { Container, Title } from './styles';
 import logoImg from '../../assets/logo.png';
 import Button from '../../components/button';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountText,
+} from './styles';
 
 interface SignInProps {
   navigation: any;
@@ -12,15 +26,44 @@ interface SignInProps {
 
 const SignIn: React.FC<SignInProps> = ({ navigation }) => {
   return (
-    <Container>
-      <Image source={logoImg} />
-      <Title> Faça seu Login </Title>
+    <>
+      {/* Por padrão, o IOS coloca o teclado em cima de toda a tela, ao invés de deslizá-la para cima */}
+      {/* Para isso é usado o KeyBoardAvoidingView */}
+      {/* No Android o comportamento de deslizamento já é o default */}
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1, flexGrow: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
 
-      <Input name="Email" icon="mail" placeholder="Email" />
-      <Input name="Password" icon="lock" placeholder="Senha" />
+            {/* View necessária apenas porque os textos no IOS não se movem usando o KeyboardAvoidingView  */}
+            <View>
+              <Title> Faça seu Login </Title>
+            </View>
 
-      <Button onPress={() => navigation.navigate('SignUp')}> Entrar </Button>
-    </Container>
+            <Input name="Email" icon="mail" placeholder="Email" />
+            <Input name="Password" icon="lock" placeholder="Senha" />
+
+            <Button onPress={() => null}> Entrar </Button>
+
+            <ForgotPassword onPress={() => null}>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountText>Criar conta</CreateAccountText>
+      </CreateAccountButton>
+    </>
   );
 };
 
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -13,3 +13,35 @@ export const Title = styled.Text`
   font-family: 'RobotoSlab-Medium';
   margin: 64px 0 24px;
 `;
+
+export const ForgotPassword = styled.TouchableOpacity`
+  margin-top: 24px;
+`;
+
+export const ForgotPasswordText = styled.Text`
+  font-size: 16px;
+  font-family: 'RobotoSlab-Regular';
+  color: #f4ede8;
+`;
+
+export const CreateAccountButton = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  padding: 16px 0;
+  background: #312e38;
+
+  border-top-width: 1px;
+  border-color: #232129;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+export const CreateAccountText = styled.Text`
+  color: #ff9000;
+  font-size: 15px;
+  font-family: 'RobotoSlab-Regular';
+  margin-left: 16px;
+`;
